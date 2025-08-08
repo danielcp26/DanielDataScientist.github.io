34fed77541cface5a27378fd003481e7c4261514
danielcp26 <168370354+danielcp26@users.noreply.github.com>
2025-08-08 12:15:29 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DanielDataScientist.github.io/', // <-- If deploying to username.github.io/REPO_NAME, replace this. For user/organization site, change to '/'
+  base: '/REPO_NAME/', // <-- If deploying to username.github.io/REPO_NAME, replace this. For user/organization site, change to '/'
 })
